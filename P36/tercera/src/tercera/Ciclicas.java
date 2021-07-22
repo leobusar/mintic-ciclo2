2af9303075c383050a96bd9cfe9fecb2df83f438
Leonardo B <leobusta@gmail.com>
2021-07-22 16:34:10 -0500

    ciclicas P36

--- P36/tercera/src/tercera/Ciclicas.java
+++ P36/tercera/src/tercera/Ciclicas.java
@@ -37,13 +37,64 @@ public class Ciclicas {
         }
     }
     
+    public static int suma(int n) {
+//        int s = 0;
+//        for (int i = 1; i <= n; i++) {
+//            s = s + i;
+//        }
+//        return s;
+        int s = 0;
+        int i = 1;
+        while (i <= n) {
+            s = s + i;
+            i++;
+        }
+        return s;
+
+    }
     
+    public static void forEach(){
+        String[] frutas = new String[]{"Tomate de árbol", "Maracuyá", "Guayaba", "Lulo", "Granadilla"};
+        
+        for(String f:frutas) { // para cada elemento f en la lista
+            if (f.equals("Lulo"))
+                break;
+            System.out.println(f);
+        }
+        for(int i=0; i < frutas.length; i++)
+            System.out.println(i+" "+frutas[i]);
+        
+//        int[] edades = new int[]{12,13,13,14,15,20,40};
+//        
+//        for(int edad:edades) { // para cada elemento f en la lista
+//            System.out.println(edad);
+//        }        
+
+    }
+
     
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        menu();
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Ingrese el valor de n: ");
+//        int n = sc.nextInt();
+//        System.out.println("Suma: "+suma(n));
+
+//        for (int i = 0, j = 10; i <= j; i++, j--) {
+//            System.out.println(i + ", " + j);
+//        }
+        
+//        for (int i = 0; i <= 30; i++) {
+//            if (i == 4) {
+//                break;
+//            }
+//            System.out.println(i);
+//        }
+//        
+        forEach();
+
         //System.out.println(minMaquina());
 //        int i = 0;
 //        while (i <= 6) {
