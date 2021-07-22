2cf5c887958086b5831877f258ce78923941cf00
Leonardo B <leobusta@gmail.com>
2021-07-22 14:31:22 -0500

    ciclicas

--- P30/tercera/src/tercera/Ciclicas.java
+++ P30/tercera/src/tercera/Ciclicas.java
@@ -14,5 +14,22 @@ import java.util.Scanner;
 public class Ciclicas {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+//        
+//        int i = 0;
+//        while (i <= 6) {
+//            System.out.println(i);
+//            i = i + 1;
+//        }
+        int i = 2; // inicializa a i en 2
+        int j = 25; // inicializa a j en 25
+        while (i < j) { // mientras i sea menor a j
+            // imprime los valores de i y j
+            System.out.println(i + ", " + j);
+            i = i * 2; // i se mult´ıplica por 2 en cada paso
+            j = j + 10; // se incrementa de 10 en 10
+        }
+// se ejecuta al terminar el ciclo
+        System.out.println("the end.");
+        System.out.println(i + ", " + j);// valores finales de i y j
     }
 }
